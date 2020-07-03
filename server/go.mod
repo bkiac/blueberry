1e689cf83b993c72d32279b934a4ab21c6c24cfa
Bence Knáb <benceknab.iac@gmail.com>
2020-07-04 00:13:32 +0200

    Add ent (#1)
    * Add database connection
    
    * Add ent

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,14 @@ module github.com/bkiac/blueberry/server
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/facebookincubator/ent v0.2.5
+	github.com/gin-gonic/gin v1.6.3
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.7.0
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,13 +1,68 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/bkiac/blueberry/server/ent"
+	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq"
+)
 
 func main() {
+	dsn := os.Getenv("DATABASE")
+	db, e := ent.Open("postgres", dsn)
+	if e != nil {
+		log.Fatalf("Failed to open database: %v", e)
+		panic(e)
+	}
+	defer db.Close()
+	// Migration
+	if e := db.Schema.Create(context.Background()); e != nil {
+		log.Fatalf("Failed to create schema resources: %v", e)
+	}
+
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"msg": "pong",
 		})
 	})
+
+	r.POST("/user", func(c *gin.Context) {
+		var body Register
+		if e := c.ShouldBindJSON(&body); e != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": e.Error()})
+			return
+		}
+
+		user, e := CreateUser(c, db, body)
+		if e != nil {
+			c.JSON(http.StatusConflict, gin.H{"error": e.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+// Register is the validator for registration POST request JSON body.
+type Register struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+// CreateUser creates a user in the database.
+func CreateUser(c context.Context, db *ent.Client, args Register) (*ent.User, error) {
+	u, e := db.User.Create().SetUsername(args.Username).SetPassword(args.Password).Save(c)
+	if e != nil {
+		return nil, e
+	}
+	return u, nil
 }
