4464a6f37c497cb32f7c92ecdfd09616160f7656
Bence Knáb <benceknab.iac@gmail.com>
2020-07-08 21:38:11 +0200

    Add GraphQL (#2)
    * Add schema file and resolver skeletons
    
    * Add create, get, get all users resolvers

--- server/go.mod
+++ server/go.mod
@@ -4,12 +4,10 @@ go 1.13
 
 require (
 	github.com/facebookincubator/ent v0.2.5
-	github.com/gin-gonic/gin v1.6.3
-	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/graph-gophers/graphql-go v0.0.0-20200622220639-c1d9693c95a6
 	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.7.0
 	github.com/stretchr/testify v1.5.1 // indirect
-	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
-	google.golang.org/protobuf v1.24.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -2,67 +2,159 @@ package main
 
 import (
 	"context"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/bkiac/blueberry/server/ent"
-	"github.com/gin-gonic/gin"
+	graphql "github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
 	_ "github.com/joho/godotenv/autoload"
 	_ "github.com/lib/pq"
 )
 
-func main() {
-	dsn := os.Getenv("DATABASE")
-	db, e := ent.Open("postgres", dsn)
-	if e != nil {
-		log.Fatalf("Failed to open database: %v", e)
-		panic(e)
-	}
-	defer db.Close()
-	// Migration
-	if e := db.Schema.Create(context.Background()); e != nil {
-		log.Fatalf("Failed to create schema resources: %v", e)
-	}
-
-	r := gin.Default()
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"msg": "pong",
-		})
-	})
-
-	r.POST("/user", func(c *gin.Context) {
-		var body Register
-		if e := c.ShouldBindJSON(&body); e != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": e.Error()})
-			return
-		}
-
-		user, e := CreateUser(c, db, body)
-		if e != nil {
-			c.JSON(http.StatusConflict, gin.H{"error": e.Error()})
-			return
-		}
-
-		c.JSON(http.StatusOK, user)
-	})
-
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-}
-
-// Register is the validator for registration POST request JSON body.
-type Register struct {
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
-}
+var db *ent.Client
+var schema *graphql.Schema
 
 // CreateUser creates a user in the database.
-func CreateUser(c context.Context, db *ent.Client, args Register) (*ent.User, error) {
+func CreateUser(c context.Context, args RegisterArgs) (*ent.User, error) {
 	u, e := db.User.Create().SetUsername(args.Username).SetPassword(args.Password).Save(c)
 	if e != nil {
 		return nil, e
 	}
 	return u, nil
+}
+
+// GetUsers returns all of the users from the database.
+func GetUsers(c context.Context) ([]*ent.User, error) {
+	us, e := db.User.Query().All(c)
+	if e != nil {
+		return nil, e
+	}
+	return us, nil
+}
+
+// GetUser returns a user from the database, if it exists.
+func GetUser(c context.Context, id int) (*ent.User, error) {
+	u, e := db.User.Get(c, id)
+	if e != nil {
+		return nil, e
+	}
+	return u, nil
+}
+
+type IDArgs struct {
+	ID string
+}
+
+type RootResolver struct{}
+
+func (r *RootResolver) Users(c context.Context) ([]*UserResolver, error) {
+	userEntities, e := GetUsers(c)
+	if e != nil {
+		return nil, e
+	}
+
+	var userResolvers []*UserResolver
+	for _, userEntity := range userEntities {
+		userResolvers = append(userResolvers, &UserResolver{&UserGraphQL{
+			ID:       userEntity.ID,
+			Username: userEntity.Username,
+		}})
+	}
+
+	return userResolvers, nil
+}
+
+func (r *RootResolver) User(c context.Context, args IDArgs) (*UserResolver, error) {
+	id64, e := strconv.ParseInt(string(args.ID), 0, 64)
+	id := int(id64)
+	if e != nil {
+		return nil, e
+	}
+
+	userEntity, e := GetUser(c, id)
+	if e != nil {
+		return nil, e
+	}
+
+	userResolver := &UserResolver{&UserGraphQL{
+		ID:       userEntity.ID,
+		Username: userEntity.Username,
+	}}
+
+	return userResolver, nil
+}
+
+type RegisterArgs struct {
+	Username string
+	Password string
+}
+
+func (r *RootResolver) Register(c context.Context, args RegisterArgs) (*UserResolver, error) {
+	userEntity, e := CreateUser(c, args)
+	if e != nil {
+		return nil, e
+	}
+
+	userResolver := &UserResolver{&UserGraphQL{
+		ID:       userEntity.ID,
+		Username: userEntity.Username,
+	}}
+
+	return userResolver, nil
+}
+
+type UserGraphQL struct {
+	ID       int
+	Username string
+}
+
+type UserResolver struct{ user *UserGraphQL }
+
+func (r *UserResolver) ID() graphql.ID {
+	return graphql.ID(strconv.Itoa(r.user.ID))
+}
+
+func (r *UserResolver) Username() string {
+	return r.user.Username
+}
+
+func main() {
+	ctx := context.Background()
+	var e error
+
+	// Sets up database connection.
+	dsn := os.Getenv("DATABASE")
+	db, e = ent.Open("postgres", dsn)
+	if e != nil {
+		log.Fatalf("Failed to open database: %v", e)
+		panic(e)
+	}
+	log.Println("💽 Database opened successfully!")
+	defer db.Close()
+	if e := db.Schema.Create(ctx); e != nil {
+		log.Fatalf("Failed to create schema resources: %v", e)
+	}
+
+	// Reads and parses the GraphQL schema.
+	bs, e := ioutil.ReadFile("./schema.graphql")
+	if e != nil {
+		log.Fatalf("Failed to read GraphQL schema: %v", e)
+	}
+	ss := string(bs)
+	schema, e = graphql.ParseSchema(ss, &RootResolver{})
+	if e != nil {
+		log.Fatalf("Failed to parse GraphQL schema: %v", e)
+	}
+
+	// Starts GraphQL server.
+	http.Handle("/", &relay.Handler{Schema: schema})
+	log.Println("📡 GraphQL server is listening!")
+	e = http.ListenAndServe(":8080", nil)
+	if e != nil {
+		log.Fatalf("Failed to start GraphQL server: %v", e)
+	}
 }
